689ba393320635d9ff2656b1522c6100970a605d
Sudeeeep <sudeepn15@gmail.com>
2022-07-19 17:02:30 +0530

    feat: add tab switching functionality

--- src/index.js
+++ src/index.js
@@ -1,15 +1,69 @@
 import './style.css';
 
 import {
+    homeContainer,
+    homeMenuBtn,
     homePage,
 } from './home';
 
+import {
+    about,
+    contact,
+    headingContainer,
+    home,
+    menu
+} from './header';
+
+import {
+    contactPage,
+    contactContainer
+} from './contact';
+
+import {
+    aboutContainer,
+    aboutLoad
+} from './about';
+import {
+    menuContainer,
+    menuPage
+} from './menu';
 
 
 const contentDiv = document.querySelector('#content');
 
+home.addEventListener('click', homePageLoad);
+contact.addEventListener('click', contactPageLoad);
+about.addEventListener('click', aboutPageLoad);
+menu.addEventListener('click', menuPageLoad);
+headingContainer.addEventListener('click', homePageLoad);
+homeMenuBtn.addEventListener('click', menuPageLoad)
+
 function homePageLoad() {
+    contactContainer.remove();
+    aboutContainer.remove();
+    menuContainer.remove();
     homePage();
+}
+
+function contactPageLoad() {
+    homeContainer.remove();
+    aboutContainer.remove();
+    menuContainer.remove();
+    contactPage();
+}
+
+function aboutPageLoad() {
+    homeContainer.remove();
+    contactContainer.remove();
+    menuContainer.remove();
+    aboutLoad();
+}
+
+function menuPageLoad() {
+    homeContainer.remove();
+    contactContainer.remove();
+    aboutContainer.remove();
+    menuPage();
 }
 
 homePageLoad();
