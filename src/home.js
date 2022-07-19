931b4b85dc44494c8bc2c3ab9563964997138f47
Sudeeeep <sudeepn15@gmail.com>
2022-07-19 17:46:34 +0530

    Revert "Merge branch 'main' of github.com:Sudeeeep/restaurant-page"
    This reverts commit 073743adb1c78b15417ddf67de5a92cc368358f6, reversing
    changes made to 689ba393320635d9ff2656b1522c6100970a605d.

--- src/home.js
+++ src/home.js
@@ -24,21 +24,6 @@ const homeImageContainer = document.createElement('div');
 function homePage() {
     headerLoad();
 
-    <<
-    << << < HEAD
-        ===
-        === =
-        const homeContainer = document.createElement('div');
-    const homeTextContainer = document.createElement('div');
-    const homeHeaderContainer = document.createElement('div');
-    const homeHeader = document.createElement('h1');
-    const homeSubHeader = document.createElement('h2');
-    const homeMenuBtnContainer = document.createElement('div');
-    const homeMenuBtn = document.createElement('button');
-    const homeImageContainer = document.createElement('div');
-
-    >>>
-    >>> > 83 f254128ecb8b6e45a01919b22bc7819aea788c
     homeContainer.classList.add('home-container');
     homeTextContainer.classList.add('home-text-container');
     homeHeaderContainer.classList.add('home-header-container');
@@ -59,8 +44,6 @@ function homePage() {
     footerLoad();
 }
 
-<<
-<< << < HEAD
 
 export {
     homePage,
@@ -72,18 +55,4 @@ export {
     homeMenuBtnContainer,
     homeMenuBtn,
     homeImageContainer,
-}; ===
-=== =
-export {
-    headerLoad,
-    footerLoad,
-    homePage,
-    contentDiv,
-}
-
-
-// //Todo: ADD ATTRIBUTION for icon TO FOOTER 
-// <a href="https://www.flaticon.com/free-icons/chai-tea" title="chai tea icons">Chai tea icons created by justicon -
-// Flaticon</a>
->>>
->>> > 83 f254128ecb8b6e45a01919b22bc7819aea788c
+};
